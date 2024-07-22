2f53007db9df1a5ef61a6b6f05940963ea395601
mateus <mateus_c_g@hotmail.com>
2024-07-22 19:16:19 +0200

    fix yaml and check k3s

--- part2/bootstrap.sh
+++ part2/bootstrap.sh
@@ -3,7 +3,23 @@
 sudo apk update
 sudo apk add curl
 curl -sfL https://get.k3s.io | sh -
-# sleep 3
+sleep 3
+
+check_kubectl() {
+    for i in {1..30}; do
+        if kubectl cluster-info >/dev/null 2>&1; then
+            echo "kubectl is working."
+            return 0
+        fi
+        echo "Waiting kubectl being ready... ($i/30)"
+        sleep 10
+    done
+    echo "Erro: kubectl not working after 5 minutes"
+    exit 1
+}
+
+# Check kubectl
+check_kubectl
 
 sudo kubectl apply -f /shared/app1.yaml
 sudo kubectl apply -f /shared/app2.yaml
