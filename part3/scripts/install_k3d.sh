1f2fba8d4a239bdf21be91bca19bc79ceae9b319
hgrranzi <vkh.mariia@gmail.com>
2024-07-27 17:43:29 +0200

    part3 - draft script for k3d setup + tmp Vagrantfile for fast vm setup

---
+++ part3/scripts/install_k3d.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install curl -y
+
+# install docker
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#install kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install k3d
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+#create cluster
+sudo k3d cluster create mycluster
