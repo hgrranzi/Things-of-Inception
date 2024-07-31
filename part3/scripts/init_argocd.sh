ccfe88682b96bfdd2855c92267c2b07097c3c677
hgrranzi <vkh.mariia@gmail.com>
2024-08-01 00:32:54 +0200

    test

--- part3/scripts/init_argocd.sh
+++ part3/scripts/init_argocd.sh
@@ -7,12 +7,11 @@ sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-
 # wait for ArgoCD to be ready
 sudo kubectl wait --for=condition=available --timeout=420s deployment/argocd-server -n argocd
 
+# apply service for port forwarding
+sudo kubectl apply -f /shared/argocd-service.yaml
+
 sudo kubectl create namespace dev
 sudo kubectl apply -f /shared/application.yaml
-
-# forward ports
-sudo kubectl port-forward svc/argocd-server 8080:443 -n argocd > /dev/null 2>&1 &
-sudo kubectl port-forward svc/myapp-service 8888:8888 -n dev > /dev/null 2>&1 &
 
 # get password
 echo "Here it is:"
