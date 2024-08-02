abfc606b792a4f3cf28451999d3e87ab6549da03
hgrranzi <vkh.mariia@gmail.com>
2024-08-02 17:53:37 +0200

    part3 - moved yaml files to confs directory

--- part3/scripts/init_argocd.sh
+++ part3/scripts/init_argocd.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# install ArgoCD
+# create namespaces
 sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+
+# apply ArgoCD
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # wait for ArgoCD to be ready
 sudo kubectl wait --for=condition=available --timeout=420s deployment/argocd-server -n argocd
 
-# apply service for port forwarding
-sudo kubectl apply -f /shared/argocd-service.yaml
+# apply node port service for ArgoCD port forwarding
+sudo kubectl apply -f ../confs/argocd-service.yaml
 
-sudo kubectl create namespace dev
-sudo kubectl apply -f /shared/application.yaml
+# apply application deployment
+sudo kubectl apply -f ../confs/application.yaml
 
-# get password
+# get ArgoCD password
 echo "Here it is:"
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo
