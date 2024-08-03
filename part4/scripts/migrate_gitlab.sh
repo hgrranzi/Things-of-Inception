db41c53f675af0ed99a255f41daac41bce26b575
hgrranzi <vkh.mariia@gmail.com>
2024-08-03 21:47:15 +0200

    part4 - draft add gitlab to working part3 lab

---
+++ part4/scripts/migrate_gitlab.sh
@@ -0,0 +1,29 @@
+# install helm
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo bash get_helm.sh
+
+# create namespace gitlab
+sudo kubectl create namespace gitlab
+
+# install gitlab with helm
+sudo helm repo add gitlab https://charts.gitlab.io
+sudo helm repo update
+
+sudo helm install gitlab gitlab/gitlab \
+  --timeout 900s \
+  --namespace gitlab \
+  --version 7.2.0 \
+  --set global.hosts.domain=local.com \
+  --set certmanager-issuer.email=iot@example.com \
+  --set global.hosts.https="false" \
+  --set gitlab-runner.install="false" \
+  --debug
+
+# waiting for gitlab to be ready
+sudo kubectl wait --for=condition=available deployment/gitlab-webservice-default -n gitlab
+
+# forward gitlab ports or apply service
+
+# clone repo from github
+
+# apply new yaml for ArgoCD
