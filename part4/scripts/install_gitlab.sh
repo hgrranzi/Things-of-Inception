34eea19f9c360dfa27950fe0fc53c9752a1bd97e
Mariia Vikhrova <79415110+hgrranzi@users.noreply.github.com>
2024-08-10 16:16:40 +0200

    Bonus (#4)
    * part4 - draft add gitlab to working part3 lab
    
    * part4 - argocd application with local repo url
    
    * part4 - todo updated
    
    * part4 - added gitlab port forwarding and getting password
    
    * part4 - added gitlab conf for minimize resource usage
    
    * part4 - draft repo migration script
    
    * part4 - some todo added
    
    * part4 - var
    
    * part4 - small sed fix
    
    * part4 - todo
    
    * part4 - adjusted repourl for argocd + todo
    
    * part4 - sed fix

---
+++ part4/scripts/install_gitlab.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# install helm
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo bash get_helm.sh
+
+# create namespace gitlab
+sudo kubectl create namespace gitlab
+
+# install gitlab with helm
+sudo helm repo add gitlab https://charts.gitlab.io
+sudo helm repo update
+
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  --timeout 900s \
+  --namespace gitlab \
+  --version 7.2.0 \
+  -f ../confs/gitlab-values.yaml \
+  --debug
+
+# add gitlab domain to hosts
+sudo echo "127.0.0.1 gitlab.local.com" | sudo tee -a "/etc/hosts" > /dev/null
+
+# waiting for gitlab to be ready
+sudo kubectl wait --for=condition=available --timeout=1800s deployment/gitlab-webservice-default -n gitlab
+
+# forward gitlab ports or apply service (could be cleaner)
+sudo kubectl port-forward svc/gitlab-webservice-default -n gitlab 80:8181 --address="0.0.0.0" >/dev/null 2>&1 &
+
+# get gitlab password
+GITLAB_PASSWORD=$(sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
+
+# save gitlab credentials
+echo "machine gitlab.local.com
+login root
+password ${GITLAB_PASSWORD}" | sudo tee /root/.netrc > /dev/null
+sudo chmod 600 /root/.netrc
+
+# print gitlab password
+echo "Here it is:"
+echo "${GITLAB_PASSWORD}"
---
+++ part4/scripts/migrate_gitlab.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# check if an argument is given
+if [ -z "$1" ]; then
+  echo "Error: No repository name provided."
+  echo "Usage: $0 <REPO_NAME>"
+  exit 1
+fi
+
+REPO_NAME=$1
+
+sudo rm -rf "${REPO_NAME}"
+
+# clone destination (fresh created) repo
+if ! sudo git clone http://gitlab.local.com/root/${REPO_NAME}.git; then
+  echo "Error: There is no repo with the name ${REPO_NAME} in GitLab, please provide an existing repo name"
+  exit 1
+fi
+
+# clone source repo
+sudo git clone https://github.com/hgrranzi/Things-of-Inception.git
+sudo mv Things-of-Inception/dev ${REPO_NAME}/
+sudo rm -rf Things-of-Inception
+
+cd "${REPO_NAME}" || {
+  echo "Error: Failed to navigate to the repository directory ${REPO_NAME}"
+  exit 1
+}
+
+sudo git add .
+sudo git commit -m "migrated"
+sudo git push
+
+cd ..
+
+# update the yaml file with the actual repo name
+sudo sed -i "s|http[^ ]*.git|http://gitlab-webservice-default.gitlab:8181/root/${REPO_NAME}.git|" ../confs/application.yaml
+
+# apply new yaml for ArgoCD
+sudo kubectl apply -f ../confs/application.yaml
